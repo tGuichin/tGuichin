d80e890bd8929ac7ba3c9528998d91ac84d0c3f8
tGuichin <tguichingen@gmail.com>
2016-06-19 15:02:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function show(element){
-    var evt = new window.MouseEvent('click', {
-        view: window,
-        bubbles: true,
-        cancelable: true
-    });
-
-    element.dispatchEvent(evt);
+function show(){
+    window.location.replace("http://www.w3schools.com");
 }
 
