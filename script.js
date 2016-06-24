8147784f883355bad60d68efc09e28ab7dc326c4
tGuichin <tguichingen@gmail.com>
2016-06-24 04:14:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function changeBackground(){
 }
 function hexValues(){
     var hexValuesVar = document.getElementById("hexShow");
-    var redHex = "FF";
+    var redHex = redvalue.toString(16);
     if((temp = redValue % 16) < 10){
         redHex.concat(temp + "");
     }
