376c31c8b6c7467dc4aa9d63b6a1ed767d54f382
tGuichin <tguichingen@gmail.com>
2016-07-06 18:34:31 -0400

    Create scriptAnim.js

---
+++ scriptAnim.js
@@ -0,0 +1,8 @@
+window.onkeypress = function(event){
+    var key = event.which;
+    var block = document.getElementById("dabMove");
+    if(key === '39'){
+        block.style.left += 10 + "px";
+        requestAnimationFrame(this);
+    }
+}
