7e94800a508cf9ea440002ac045f756b800b9a49
tGuichin <tguichingen@gmail.com>
2016-07-07 04:26:11 -0400

    Update scriptAnim.js

--- scriptAnim.js
+++ scriptAnim.js
@@ -1,7 +1,7 @@
 function keyPress(event){
     var key = String.fromCharCode(event.keyCode);
     var block = document.getElementById("dabMove");
-    
+    var dab = document.getElementById("dabMove").addEventListener("keydown", keyPress,false);
     if(key == "d"){
         block.style.left += 10 + "px";
         console.log("clicking right" + key);
