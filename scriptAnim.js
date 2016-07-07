a2e3979ab972cbe86662e9b6eec716da6977ef74
tGuichin <tguichingen@gmail.com>
2016-07-07 04:41:49 -0400

    Update scriptAnim.js

--- scriptAnim.js
+++ scriptAnim.js
@@ -1,6 +1,6 @@
 window.onkeydown = keyPress;
 function keyPress(event){
-    var key = event.keyCode;
+    var key = event.keyCode || event.which;
     var block = document.getElementById("dabMove");
     var dab = document.getElementById("dabMove").addEventListener("keydown", keyPress,false);
     if(key == 39){
