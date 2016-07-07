3b2304fccbac0fcadb08f25e71f7a762f374ce81
tGuichin <tguichingen@gmail.com>
2016-07-07 04:13:09 -0400

    Update scriptAnim.js

--- scriptAnim.js
+++ scriptAnim.js
@@ -2,7 +2,7 @@ function keyPress(event){
     var key = (window.event) ? event.keyCode : event.which;
     var block = document.getElementById("dabMove");
     
-    if(key == 39){
+    if(event.keyCode == 39){
         block.style.left += 10 + "px";
         console.log("clicking right" + key);
     }
